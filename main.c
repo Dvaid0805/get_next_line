74c001b3527a131b171f33341d6d010d8a1e443f
Davyd Bredykhin <dbredykh@c2r2s3.42malaga.com>
2023-06-10 22:18:13 +0200

    knowleges after evaluation

--- main.c
+++ main.c
@@ -5,23 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbredykh <dbredykh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/01 13:40:11 by dbredykh          #+#    #+#             */
-/*   Updated: 2023/06/10 16:48:51 by dbredykh         ###   ########.fr       */
+/*   Created: 2023/06/10 21:23:11 by dbredykh          #+#    #+#             */
+/*   Updated: 2023/06/10 22:17:35 by dbredykh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+void	ft_leaks(void)
+{
+	system ("leaks -q test");
+}
+// pa checkear lick de memoria
+
 int	main(void)
 {
-	int			fd;
-	const char	*filename;
-	ssize_t		test;
+	int		fd;
+	char	*line;
 
-	filename = "test.txt";
-	fd = open(filename, O_RDONLY);
-	printf("main: %s\n", get_next_line(fd));
-	/* close(fd); */
-	printf("main: %s\n", get_next_line(fd));
+	atexit(ft_leaks);
+	fd = open("hola.txt", 0);
+	get_next_line(fd);
+	printf("main: %s\n", line);
+	free(line);
 	return (0);
 }
+
+/* fd = open("hola.txt", 0); en vez de O_RDONLY */
+/* char *line = get_next_line(0);   */
