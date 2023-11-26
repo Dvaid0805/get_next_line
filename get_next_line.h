a2e0074585083d9410a33627c0adec4a951c51f5
Dvaid0805 <correodemimobil@gmail.com>
2023-11-26 14:48:57 +0100

    feat: header file was created

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dbredykh <dbredykh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 21:41:17 by dbredykh          #+#    #+#             */
-/*   Updated: 2023/11/25 18:37:58 by dbredykh         ###   ########.fr       */
+/*   Updated: 2023/11/26 14:37:57 by dbredykh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,8 @@
 # include <string.h>
 
 char	*get_next_line(int fd);
+char	*ft_strrchar(char *s, int c);
+int		ft_strlen(char *str);
+char	*ft_strjoin(char *stash, char *buff);
 
 #endif
